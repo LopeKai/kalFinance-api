c3348279ed7bc1343811783e1f37ba74b28d2922
Kaique araujo lopes <kaiquearaujo246@hotmail.com>
2026-05-03 15:18:57 -0300

    chore: create decorators ActiveUserId and IsPublic

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -9,6 +9,7 @@ import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { env } from 'process';
+import { IS_PUBLIC_KEY } from 'src/shared/decorators/IsPublic';
 
 // Guardas(middleware) de autenticação e autorização, interceptação de requisições, manipulação de erros 
 
@@ -17,7 +18,7 @@ export class AuthGuard implements CanActivate {
     constructor(private readonly jwtService: JwtService, private reflector: Reflector) { }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        const isPublic = this.reflector.getAllAndOverride<boolean>("IS_PUBLIC", [context.getClass(), context.getHandler()])
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [context.getClass(), context.getHandler()])
 
         if (isPublic) return true;
 
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,16 +1,15 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
-import type { Request } from 'express';
-import { AuthGuard } from '../auth/auth.guard';
+import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
 
 // Requisições HTTP, validação, autenticação, autorização
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  me(@Req() request: any) {
-    return this.usersService.getUserById(request.userId);
+  me(@ActiveUserId() userId: string) {
+    return this.usersService.getUserById(userId);
   }
 }
 
